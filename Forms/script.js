fc4074c1f34687167cc00c6417d970df89493217
annenorenes <anne.norenes@icloud.com>
2025-12-05 14:49:27 +0100

    wd

---
+++ Forms/script.js
@@ -0,0 +1,16 @@
+const skjerm = document.getElementById("skjerm");
+
+skjerm.addEventListener = ("submit", function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value; //vi bruker value til å oppdattere verdien
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const kjonn = document.querySelector('input[name="kjonn"]:checked').value;
+    const interesserSelect = document.getElementById('interesser');
+    const interesser = Array.from(interesserSelect.selectedOptions).map(option => option.value);
+
+
+})
+
+
