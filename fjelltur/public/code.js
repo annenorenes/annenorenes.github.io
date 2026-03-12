1ca874d1e5a2c06cf40a64a1117581a7522b8cb1
annenorenes <anne.norenes@icloud.com>
2026-03-12 22:37:16 +0100

    arbeid kveld

--- fjelltur/public/code.js
+++ fjelltur/public/code.js
@@ -1,10 +1,31 @@
-async function fetchData() {
-    const response = await fetch('http://localhost:3000/api/fjell_info');
-    const data = await response.json();
+// Vi skal lage en oversikt over fjellene
+
+async function hentFjell() {
+    const respons = await fetch("http://localhost:3000/api/fjell_info");
+    const data = await respons.json();
     console.log(data);
-    
-    
-    
+
+    const utskrift = document.querySelector("#fjellContainer");
+    utskrift.innerHTML = "";
+
+    for (let i = 0; i < data.length; i++) {
+        let fjell = data[i];
+
+        let div = document.createElement("div");
+        div.className = "fjell-card";
+
+        div.innerHTML = `
+            <h2>${fjell.fjellnavn}</h2>
+            <p>Høyde: ${fjell.hoyde} meter</p>
+            <p>Beskrivelse: ${fjell.beskrivelse}</p>
+            <img src="/bilder/${fjell.foto}" alt="${fjell.fjellnavn}">
+        `;
+
+        utskrift.appendChild(div);
+    }
 }
 
-fetchData();
+hentFjell();
+
+
+//http://localhost:3000/api/fjell_info
